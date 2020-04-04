9ba97c657755d1d5f9fd3de39d5345a328d7b21a
github username <github email address>
2020-04-04 16:12:13 -0600

    Add functioning auth

--- queries.js
+++ queries.js
@@ -2,9 +2,9 @@ const connection = require('./knexfile')[process.env.NODE_ENV || 'development']
 const database = require('knex')(connection)
 
 module.exports = {
-    listAll() {
+    listAll(user) {
         return database('recipes')
-        .where('user_id', 4)
+        .where('user_id', user.id)
     },
 
 }
